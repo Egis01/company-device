437925509cc33437a1e02ed603a9f6e20611ee0b
unknown <neatsimenu@gmail.com>
2019-12-04 06:15:34 +0200

    klasems pridejau konstruktorius ir getterius, sukuriau duomenu faila

--- src/Company.java
+++ src/Company.java
@@ -1,2 +1,29 @@
 public class Company {
+
+    String name, address;
+    ProductionType productionType;
+    Device devices;
+
+    public Company(String name, String address, ProductionType productionType, Device devices) {
+        this.name = name;
+        this.address = address;
+        this.productionType = productionType;
+        this.devices = devices;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public ProductionType getProductionType() {
+        return productionType;
+    }
+
+    public Device getDevices() {
+        return devices;
+    }
 }
--- src/Device.java
+++ src/Device.java
@@ -1,2 +1,30 @@
 public class Device {
+
+    String name;
+    double price;
+    int quantity;
+    String description;
+
+    public Device(String name, double price, int quantity, String description) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
